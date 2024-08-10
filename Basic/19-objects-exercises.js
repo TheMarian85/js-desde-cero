415b335bfb085258c03c1c704c97cf6870298dc5
Mariano Koci <mariano.koci@seciu.edu.uy>
2024-08-10 20:26:10 -0300

    Ejercicios objetos

--- Basic/19-objects-exercises.js
+++ Basic/19-objects-exercises.js
@@ -6,20 +6,61 @@ https://www.twitch.tv/videos/2218300512?t=00h17m10s
 
 // 1. Crea un objeto con 3 propiedades
 
+let myObject = {
+    myName : "Mariano",
+    myAge : "39",
+    myMail: "mariano.koci@gmail.com"
+}
+
 // 2. Accede y muestra su valor
+
+console.log(`Mi nombre es: ${myObject.myName}`)
+console.log(`Mi edad es: ${myObject.myAge}`)
+console.log(`Mi email es: ${myObject.myMail}`)
 
 // 3. Agrega una nueva propiedad
 
+myObject.job = 'Developer'
+console.table(myObject)
+
 // 4. Elimina una de las 3 primeras propiedades
+
+delete myObject.myAge
+console.table(myObject)
 
 // 5. Agrega una función e invócala
 
+myObject.showMessage = function(){
+    console.log('Esta es una función dentro de un objeto')
+}
+myObject.showMessage()
+
+
 // 6. Itera las propiedades del objeto
+
+for (let key in myObject) {
+    console.log(key + ": " + myObject[key])
+}
 
 // 7. Crea un objeto anidado
 
+myObject.work = {
+    jobName : 'Developer',
+    place : 'Udelar'
+}
+
+console.table(myObject)
+console.table(myObject.work)
+
 // 8. Accede y muestra el valor de las propiedades anidadas
+
+console.log(myObject.work.jobName)
+console.log(myObject.work.place)
 
 // 9. Comprueba si los dos objetos creados son iguales
 
+console.log(myObject === myObject.job ? 'Los objetos son iguales' : 'Los objetos son distintos')
+
 // 10. Comprueba si dos propiedades diferentes son iguales
+
+console.log(myObject.job === myObject.work.jobName ? 'Las propiedades son iguales' : 'Las propiedades son distintas')
